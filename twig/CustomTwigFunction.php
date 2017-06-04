dc13f607bb477032fd620f8a4927ed99568d0dfc
Ole Vik <OleVik@users.noreply.github.com>
2017-06-05 00:09:29 +0200

    Optimize TwigExtension

--- twig/CustomTwigFunction.php
+++ twig/CustomTwigFunction.php
@@ -7,102 +7,95 @@ use \Twig_Extension;
 use \Twig_SimpleFunction;
 
 /**
- * ARRAY OF CUSTOM TWIG FUNCTIONS
+ * [Short description]
+ *
+ * [Long description]
+ *
+ * Class WebpackTwigFunction
+ * @package Grav\Plugin
+ * @return string|void Webpack assets
+ * @license MIT License by YourNameHere
  */
-
 class WebpackTwigFunction extends Twig_Extension
 {
 
+    /**
+     * Environment-configuration
+     * @var string
+     */
     protected $mode;
 
+    /**
+     * Instantiate WebpackTwigFunction-class
+     */
     public function __construct()
     {
         $this->mode = Grav::instance()['config']['plugins.webpackdevtools']['mode'];
     }
 
     /**
-     * RETURNS EXTENSION NAME
+     * Declare extension name for backwards-compatibility
+     * @return string Extension name
      */
-
     public function getName()
     {
         return 'WebpackTwigFunction';
     }
 
     /**
-     * REGISTER TWIG FUNCTIONS
+     * Returns webpack_asset-function
+     * @return Twig_SimpleFunction
      */
-
     public function getFunctions()
     {
         return [
           new Twig_SimpleFunction('webpack_asset', [$this, 'webpackAssets'], ['is_safe' => ['html']])
         ];
     }
-
     /**
-    * WEBPACK ASSETS FUNCTION
-    *
-    * @param $file [string] the asset file name
-    *
-    * @param $inline [boolean] 'true' if inline (default is 'false')
-    */
-
+     * Add Webpack assets
+     * @param string $file   [description]
+     * @param boolean $inline [description]
+     * @return string|void [description]
+     */
     public function webpackAssets($file, $inline = false)
     {
-        $stringParts = explode('.', $file);
-        $file_name = $stringParts[0];
-
-        if (preg_match('/(\.css|\.js)$/i', $file)) {
-            $file_ext = $stringParts[1];
-        } else {
-            $file_ext = null;
+        $locator = Grav::instance()['locator'];
+        $pathParts = pathinfo($file);
+        $filename = $pathParts['filename'];
+        if (isset($pathParts['extension'])) {
+            $fileExt = $pathParts['extension'];
         }
 
-        /**
-         * If environment is development
-         */
-
+        /* Check environments */
         if ($this->mode == 'development') {
-            $dev_path = 'http://localhost:3000/assets/js/';
-            $asset_path = $dev_path . $file_name . '.js';
+            $devPath = 'http://localhost:3000/assets/js/';
+            $assetPath = $devPath . $filename . '.js';
 
-            return '<script src="' . $asset_path . '" async></script>';
-        }
-
-        /**
-         * If environment is production
-         */
-
-        if ($this->mode == 'production') {
-            $webpack_assets =  'theme://assets/webpack-assets.json';
+            return '<script src="' . $assetPath . '" async></script>';
+        } elseif ($this->mode == 'production') {
+            $webpack_assets = $locator->findResource('theme://assets/webpack-assets.json', true);
             if (file_exists($webpack_assets)) {
-                $assets_array = json_decode(file_get_contents($webpack_assets), true);
-                $asset_path = $assets_array[$file_name];
-
-                // If inline is true
+                $assetsArray = json_decode(file_get_contents($webpack_assets), true);
+                $assetPath = $assetsArray[$filename];
+                $css = $locator->findResource('theme://' . $assetPath['css'], true);
+                $js = $locator->findResource('theme://' . $assetPath['js'], true);
                 if ($inline) {
-                    // If file extension is null
-                    if (!$file_ext) {
+                    if (!$fileExt) {
                         return;
-                    // If file extension is css
-                    } elseif ($file_ext == 'css') {
-                        return '<style>' . file_get_contents('theme://' . $asset_path['css']) . '</style>';
-                    // If file extension is js
-                    } elseif ($file_ext == 'js') {
-                        return '<script>' . file_get_contents('theme://' . $asset_path['js']) . '</script>';
+                    } elseif ($fileExt == 'css') {
+                        return '<style>' . file_get_contents($css) . '</style>';
+                    } elseif ($fileExt == 'js') {
+                        return '<script>' . file_get_contents($js) . '</script>';
                     }
-                // If inline is false
                 } else {
-                    // If file extension is null
-                    if (!$file_ext) {
-                        return '<link rel="stylesheet" href="theme://' . $asset_path['css'] . '"><script src="theme://' . $asset_path['js'] . '" async></script>';
-                    // If file extension is css
-                    } elseif ($file_ext == 'css') {
-                        return '<link rel="stylesheet" href="theme://' . $asset_path['css'] . '">';
-                    // If file extension is js
-                    } elseif ($file_ext == 'js') {
-                        return '<script src="theme://' . $asset_path['js'] . '" async></script>';
+                    if (!isset($fileExt)) {
+                        Grav::instance()['assets']->addCss('theme://' . $assetPath['css']);
+                        Grav::instance()['assets']->addJs('theme://' . $assetPath['js']);
+                    } elseif ($fileExt == 'css') {
+                        Grav::instance()['assets']->addCss('theme://' . $assetPath['css']);
+                    } elseif ($fileExt == 'js') {
+                        Grav::instance()['assets']->addJs('theme://' . $assetPath['js']);
                     }
                 }
             }
